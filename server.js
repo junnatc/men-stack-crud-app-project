11000531987e4092ebddfc950e9471e8a299915c
junnatc <junnatchowdhury@gmail.com>
2024-09-13 14:05:28 -0400

    path

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+const path = require('path');
+app.set('views', path.join(__dirname, 'views'));
+
+
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Failed to connect to MongoDB', err));
